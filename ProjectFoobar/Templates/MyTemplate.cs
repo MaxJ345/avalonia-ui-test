0d2acfe632d8e8da7f88722415adaa429de5d2e8
Maxwell Jacob <maxj345@proton.me>
2024-11-12 15:05:41 -0500

    Add the template. Test the template.

---
+++ ProjectFoobar/Templates/MyTemplate.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace ProjectFoobar.Templates;
+
+public class MyTemplate : TemplatedControl
+{
+    public MyTemplate()
+    {
+    }
+}
---
+++ ProjectFoobar/Views/TestControl.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ProjectFoobar;
+
+public partial class TestControl : UserControl
+{
+    public TestControl()
+    {
+        InitializeComponent();
+    }
+}
